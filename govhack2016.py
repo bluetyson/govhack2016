423cea8506b79a097799d617c6ade8c3ab2a9793
max-ar <max@rtechservices.io>
2016-07-30 20:26:43 +1000

    sadf

--- govhack2016.py
+++ govhack2016.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-import os
-from os import path
+import json
+from datalogic import competition
 
 app = Flask(__name__)
 
@@ -25,13 +25,26 @@ def biz_about():
     return render_template('about.html')
 
 
-@app.route('/query/<string:type>',methods=['POST'])
-def query(type):
-    if type == 'competition':
-        return 'FRODO BAGGINSSSSS'#competition_algorithm()
-    if type == 'avgperson':
+@app.route('/query/<string:querytype>', methods=['POST'])
+def query(querytype):
+    if querytype == 'competition':
+        return {}
+    if querytype == 'avgperson':
         return {}
 
+@app.route('/test/query/<string:querytype>', methods=['GET'])
+def query_test(querytype):
+    querytype = querytype.lower
+    # val = None
+    if querytype == 'competition':
+        val = json.dumps(competition.get_competition("Agriculture, Forestry and Fishing", 101011001))
+    elif querytype == 'avgperson':
+        val = json.dumps(competition.get_competition("Agriculture, Forestry and Fishing", 101011001))
+    elif querytype == 'laboravail':
+        val = json.dumps(competition.get_competition("Agriculture, Forestry and Fishing", 101011001))
+    else:
+        val = "Error"
+    return val
 
 if __name__ == '__main__':
     app.run(debug=True)
