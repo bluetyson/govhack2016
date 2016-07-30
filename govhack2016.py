50da292d470df1a6f9752c32842ffd4af224252b
max-ar <max@rtechservices.io>
2016-07-30 11:58:25 +1000

    nasty

--- govhack2016.py
+++ govhack2016.py
@@ -3,18 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-#TODO: change hello world route
-@app.route('/')
-def hello_world():
-    return render_template('frontpage.html')
+# TODO: change hello world route
+# @app.route('/')
+# def hello_world():
+#     return render_template('frontpage.html')
 
+@app.route('/')
 @app.route('/form')
 def biz_form():
     return render_template('form.html')
+
 
 @app.route('/result')
 def biz_result():
     return render_template('result.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(app.run(host='0.0.0.0'))
