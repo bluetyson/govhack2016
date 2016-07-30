2ce283f4ce4f0414bbf20a9477659081fcfc9028
max-ar <max@rtechservices.io>
2016-07-30 16:53:00 +1000

    added deps

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ I should write something useful here.
 # To run in production
 Run this command in project root;
 `gunicorn -w 4 govhack2016:app -b 127.0.0.1:5000`
+
+# Operating system Deps
+`sudo apt install python3-dev libpq-dev` - Ubuntu/debian
---
+++ data-logic/competition.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+# returns [TOTAL 2014, TOTAL 2013, TOTAL 2012, X Employees 2014, X Employees 2013, X Employees 2012] where X is the stated number of employees, if given
+# If employees not specified, will only return [TOTAL 2014, TOTAL 2013, TOTAL 2012]
+
+
+def get_competition(industry, SA_code, employees="NA"):
+    conn = psycopg2.connect(dbname="govhack", user="govhack", password="govhack", host="107.155.108.51")
+    cur = conn.cursor()
+
+    query = "SELECT * FROM business_employee_counts WHERE field_1='" + industry + "' AND field_2='" + SA_code + "'"
+    cur.execute(query)
+    for line in cur.fetchall():
+        num_businesses = [int(line[8])*1000, int(line[14])*1000, int(line[20])*1000]
+        columns = []
+        if employees == 0:
+            columns = [3, 9, 15]
+        elif employees in range(1, 5):
+            columns = [4, 10, 16]
+        elif employees in range(5, 20):
+            columns = [5, 11, 17]
+        elif employees in range(20, 200):
+            columns = [6, 12, 18]
+        elif employees > 200:
+            columns = [7, 13, 19]
+        for i in columns:
+            num_businesses.append(int(line[i])*1000)
+        print(num_businesses)
+
+    cur.close()
+    conn.close()
