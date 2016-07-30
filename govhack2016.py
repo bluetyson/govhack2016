505eb21335712f11b9cfb7517cc0c68ad45cb888
max-ar <max@rtechservices.io>
2016-07-30 20:04:35 +1000

    modifyed router

--- govhack2016.py
+++ govhack2016.py
@@ -5,14 +5,16 @@ from os import path
 app = Flask(__name__)
 
 
-#TODO: change hello world route
+
 @app.route('/')
 def hello_world():
     return render_template('frontpage.html')
 
+
 @app.route('/form')
 def biz_form():
     return render_template('form.html')
+
 
 @app.route('/result')
 def biz_result():
@@ -22,6 +24,7 @@ def biz_result():
 def biz_about():
     return render_template('about.html')
 
+
 @app.route('/query/<string:type>',methods=['POST'])
 def query(type):
     if type == 'competition':
@@ -30,17 +33,5 @@ def query(type):
         return {}
 
 
-
-# additional files to check for reloader
-extra_dirs = ['templates','static']
-extra_files = extra_dirs[:]
-for extra_dir in extra_dirs:
-    for dirname, dirs, files in os.walk(extra_dir):
-        for filename in files:
-            filename = path.join(dirname, filename)
-            if path.isfile(filename):
-                extra_files.append(filename)
-
-
 if __name__ == '__main__':
-    app.run(use_reloader=True,extra_files=extra_files)
+    app.run(debug=True)
