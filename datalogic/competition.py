abe4787c28691980b802a8056365f188903a45ed
max-ar <max@rtechservices.io>
2016-07-30 20:03:22 +1000

    test and data logic added

--- datalogic/competition.py
+++ datalogic/competition.py
@@ -3,10 +3,12 @@ import psycopg2
 # returns [TOTAL 2014, TOTAL 2013, TOTAL 2012, X Employees 2014, X Employees 2013, X Employees 2012] where X is the stated number of employees, if given
 # If employees not specified, will only return [TOTAL 2014, TOTAL 2013, TOTAL 2012]
 
-
-def get_competition(industry, SA_code, employees="NA"):
+# requires pre prossesing for SA2 code & industry
+def get_competition(industry, SA_code, employees=0):
     conn = psycopg2.connect(dbname="govhack", user="govhack", password="govhack", host="107.155.108.51")
     cur = conn.cursor()
+
+    SA_code = str(SA_code)
 
     query = "SELECT * FROM business_employee_counts WHERE field_1='" + industry + "' AND field_2='" + SA_code + "'"
     cur.execute(query)
@@ -23,9 +25,15 @@ def get_competition(industry, SA_code, employees="NA"):
             columns = [6, 12, 18]
         elif employees > 200:
             columns = [7, 13, 19]
-        for i in columns:
-            num_businesses.append(int(line[i])*1000)
-        print(num_businesses)
+
+
+        return dict(zip(num_businesses, columns))
+
+
+
+
 
     cur.close()
     conn.close()
+
+
